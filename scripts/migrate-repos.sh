e000304508b1241b49766ea9531aed910e067f24
Meena-1601 <meenasubasri1998@gmail.com>
2026-08-07 14:11:36 +0530

    Add script to migrate repositories between GitHub accounts

---
+++ scripts/migrate-repos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SOURCE_BASE="https://github.com/Meena-1601"
+TARGET_BASE="https://github.com/meenasubashri1998-code"
+
+repos=(
+  python-app-deploy
+  aws_resource_list.sh
+  Jenkins-pipeline
+  git-pjt
+)
+
+for repo in "${repos[@]}"
+do
+  echo "Migrating ${repo} ..."
+
+  git clone --mirror "$SOURCE_BASE/${repo}.git"
+
+  cd "${repo}.git"
+
+  git remote set-url origin "$TARGET_BASE/${repo}.git"
+
+  git push --mirror
+
+  cd ..
+  rm -rf "${repo}.git"
+
+  echo "$repo migrated successfully"
+done
